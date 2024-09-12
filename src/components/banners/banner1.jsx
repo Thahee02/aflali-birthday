d69fad35517d5c63c4e2203d0c6c8371774ccca7
Thahee02 <developerthahee@gmail.com>
2024-09-12 16:59:17 +0530

    animate.css init

--- src/components/banners/banner1.jsx
+++ src/components/banners/banner1.jsx
@@ -1,11 +1,12 @@
 import Banner1 from '../../assets/banner1.jpg'
+import 'animate.css';
 
 const BannerSection1 = () => {
     return ( 
         <div className="flex items-center px-16 bg-blue-900 w-full h-screen relative overflow-hidden select-none" style={{backgroundImage: 'url('+ Banner1 +')'}}>
             <h1 className="text-6xl font-bold text-white max-w-[900px]">WISH YOU MANY MORE HAPPY RETURNS OF THE DAY <br /><span className="text-yellow-400 text-8xl">AFLALI</span></h1>
             <div className='absolute bottom-10 w-full flex'>
-                <div className='flex items-center gap-2 px-4 py-2 border-2 rounded-3xl text-white font-bold drop-shadow-2xl'>
+                <div className='flex items-center gap-2 px-4 py-2 border-2 rounded-3xl text-white font-bold drop-shadow-2xl animate__animated animate__pulse animate__infinite'>
                     <span>SCROLL DOWN</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 16.175l3.9-3.875q.275-.275.688-.288t.712.288q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062t-.375-.062t-.325-.213l-4.6-4.6q-.275-.275-.288-.687T6.7 12.3q.275-.275.7-.275t.7.275zm0-6L15.9 6.3q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062t-.375-.062t-.325-.213L6.7 7.7q-.275-.275-.288-.687T6.7 6.3q.275-.275.7-.275t.7.275z"></path></svg>
                 </div>
--- src/components/banners/banner2.jsx
+++ src/components/banners/banner2.jsx
@@ -1,4 +1,5 @@
 import PhotoFrame from '../../assets/photo-frame.png'
+import 'animate.css';
 
 const BannerSection2 = () => {
     return ( 
@@ -6,7 +7,7 @@ const BannerSection2 = () => {
             <img src={PhotoFrame} alt="" className='absolute top-14 xl:left-16 w-[1400px]' />
             <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold py-8 z-10">MEMORIES WITH "US🥰"</h1>
             <div className='absolute bottom-5 w-full flex justify-center lg:justify-end px-16'>
-                <div className='flex items-center gap-2 px-4 py-2 border-2 rounded-3xl text-white font-bold drop-shadow-2xl'>
+                <div className='flex items-center gap-2 px-4 py-2 border-2 rounded-3xl text-white font-bold drop-shadow-2xl animate__animated animate__pulse animate__infinite'>
                     <span>SCROLL DOWN</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 16.175l3.9-3.875q.275-.275.688-.288t.712.288q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062t-.375-.062t-.325-.213l-4.6-4.6q-.275-.275-.288-.687T6.7 12.3q.275-.275.7-.275t.7.275zm0-6L15.9 6.3q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062t-.375-.062t-.325-.213L6.7 7.7q-.275-.275-.288-.687T6.7 6.3q.275-.275.7-.275t.7.275z"></path></svg>
                 </div>
