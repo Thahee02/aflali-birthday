98f4bc6e5126a95dcf18762237c7d59dab4ef52f
Thahee02 <developerthahee@gmail.com>
2024-09-12 16:28:49 +0530

    design: memories with us

--- src/components/banners/banner2.jsx
+++ src/components/banners/banner2.jsx
@@ -1,7 +1,10 @@
+import PhotoFrame from '../../assets/photo-frame.png'
+
 const BannerSection2 = () => {
     return ( 
-        <div>
-            hi
+        <div className="w-full h-screen bg-yellow-600 select-none relative overflow-hidden">
+            <img src={PhotoFrame} alt="" className='absolute top-14 left-16 w-[1400px]' />
+            <h1 className="text-center text-7xl font-bold py-8 z-10">MEMORIES WITH "US🥰"</h1>
         </div>
      );
 }
