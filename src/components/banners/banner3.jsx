e16e3a3fb90a5e716fb4833fcce73e4e565a7bbb
Thahee02 <developerthahee@gmail.com>
2024-09-13 12:21:36 +0530

    fix: carousel

--- src/components/banners/banner3.jsx
+++ src/components/banners/banner3.jsx
@@ -7,7 +7,7 @@ const BannerSection3 = () => {
     const responsive = {
         abovexl: {
             breakpoint: { max: 4000, min: 1536 },
-            items: 3
+            items: 4
         },
         xl: {
             breakpoint: { max: 1536, min: 1280 },
@@ -15,11 +15,11 @@ const BannerSection3 = () => {
         },
         lg: {
             breakpoint: { max: 1280, min: 1024 },
-            items: 2
+            items: 3
         },
         md: {
             breakpoint: { max: 1024, min: 768 },
-            items: 1
+            items: 2
         },
         sm: {
             breakpoint: { max: 768, min: 0 },
@@ -31,8 +31,33 @@ const BannerSection3 = () => {
         <div className='bg-blue-950 pb-12'>
             <h1 className="text-center text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold py-8 z-10">WISHES OF "US🥰"</h1>
             <div className='w-full flex justify-center items-center mt-10'>
-                <Carousel responsive={responsive} infinite={true}  centerMode={false} autoPlay={true} autoPlaySpeed={7500} transitionDuration={1000} className='px-5'>               
-                    <div className='w-96  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                <Carousel responsive={responsive} infinite={true}  centerMode={false} autoPlay={true} autoPlaySpeed={7500} transitionDuration={1000} className='max-lg:px-14 max-xl:px-4 px-12'>               
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                        <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
+                        <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
+                        <q className='text-gray-400 text-center text-sm bg-black p-2 rounded'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
+                    </div>
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                        <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
+                        <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
+                        <q className='text-gray-300 text-center text-sm'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
+                    </div>
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                        <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
+                        <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
+                        <q className='text-gray-300 text-center text-sm'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
+                    </div>
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                        <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
+                        <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
+                        <q className='text-gray-300 text-center text-sm'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
+                    </div>
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
+                        <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
+                        <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
+                        <q className='text-gray-300 text-center text-sm'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
+                    </div>
+                    <div className='w-80  border-2 rounded-xl text-white flex flex-col items-center py-5 px-4'>
                         <div className='w-20 h-20 rounded-full bg-center bg-cover shrink-0' style={{backgroundImage: 'url(' + ThaheeImage + ')'}}></div>
                         <h1 className='font-bold my-3 text-yellow-400'>HATHIL THAHEE</h1>
                         <q className='text-gray-300 text-center text-sm'>Happy Birthday mcn 🎉, Aflali! Even though we only spent 10 days together, those days were truly unforgettable. You&apos;ve been such a caring and kind friend, and I&apos;m so grateful for the time we shared. Wishing you a birthday filled with love, laughter, and all the happiness you deserve. May this year bring you endless joy and wonderful memories! 🥳🎂 Stay amazing, my friend!</q>
